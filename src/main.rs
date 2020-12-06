35096533164354164450d13a5e4c282f67e6aee4
Bernardo Meurer <bernardo@meurer.org>
2020-12-05 19:54:55 -0800

    main: store scores in chromosomes

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,8 @@
 use rand::prelude::*;
+use std::cmp::Ordering;
 use std::ops::Mul;
 
-#[derive(Clone, Debug)]
-struct Chromosome {
-    solution: Vec<u8>,
-}
-
-impl Chromosome {
-    fn random(len: usize) -> Self {
-        Chromosome {
-            solution: std::iter::repeat_with(random).take(len).collect(),
-        }
-    }
-
-    fn distance(&self, goal: &[u8]) -> u32 {
-        triple_accel::hamming(&self.solution, goal)
-    }
-
-    fn mutate(&mut self) {
-        use rand::distributions::Uniform;
-
-        let mut rng = thread_rng();
-
-        let index_distribution = Uniform::from(0..self.solution.len());
-        let rand_maybe = rng.gen_range(0, 100);
-
-        if rand_maybe <= 40 {
-            self.solution[index_distribution.sample(&mut rng)] = random();
-            self.solution[index_distribution.sample(&mut rng)] = random();
-        }
-    }
-}
-
-impl std::fmt::Display for Chromosome {
-    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(fmt, "\"{}\"", String::from_utf8_lossy(&self.solution))
-    }
-}
-
-impl Mul for Chromosome {
-    type Output = (Self, Self);
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        let mut father = self.solution;
-        let mut mother = rhs.solution;
-
-        assert_eq!(father.len(), mother.len());
-        let len = father.len();
-
-        let mut rng = thread_rng();
-        let cut_a: usize = rng.gen_range(0, len);
-        let cut_b: usize = rng.gen_range(0, len);
-        let min = cut_a.min(cut_b);
-        let max = cut_a.max(cut_b);
-
-        let mut son = vec![0; len];
-        let mut daughter = vec![0; len];
-
-        son[min..max].copy_from_slice(&mother[min..max]);
-        daughter[min..max].copy_from_slice(&father[min..max]);
-
-        mother.rotate_left(max);
-        father.rotate_left(max);
-
-        son[max..len].copy_from_slice(father.drain(0..(len - max)).as_slice());
-        son[0..min].copy_from_slice(father.drain(0..min).as_slice());
-
-        daughter[max..len].copy_from_slice(mother.drain(0..(len - max)).as_slice());
-        daughter[0..min].copy_from_slice(mother.drain(0..min).as_slice());
-
-        (
-            Chromosome { solution: son },
-            Chromosome { solution: daughter },
-        )
-    }
-}
-
-fn main() {
-    use indicatif::{ProgressBar, ProgressStyle};
-    use rayon::prelude::*;
-
-    let pb = ProgressBar::new_spinner();
-    pb.set_style(
-        ProgressStyle::default_spinner().template("{elapsed_precise} | {per_sec} | {wide_msg}"),
-    );
-
-    let goal: Vec<u8> = b"
+static GOAL: &[u8] = b"
 Twas brillig, and the slithy toves
       Did gyre and gimble in the wabe:
 All mimsy were the borogoves,
@@ -120,34 +37,149 @@ O frabjous day! Callooh! Callay!\"
       Did gyre and gimble in the wabe:
 All mimsy were the borogoves,
       And the mome raths outgrabe.
-    "
-    .to_vec();
+";
+
+#[derive(Clone, Debug, Eq)]
+struct Chromosome {
+    solution: Vec<u8>,
+    cost: u32,
+}
+
+impl Chromosome {
+    fn random(len: usize) -> Self {
+        let solution: Vec<u8> = std::iter::repeat_with(random).take(len).collect();
+        let cost = Self::hamming(&solution);
+        Chromosome { solution, cost }
+    }
+
+    fn hamming(a: &[u8]) -> u32 {
+        triple_accel::hamming(a, &GOAL)
+    }
+
+    fn mutate(&mut self) {
+        use rand::distributions::Uniform;
+
+        let mut rng = thread_rng();
+
+        let mut mutated = self.solution.clone();
+        let index_distribution = Uniform::from(0..self.solution.len());
+        let rand_maybe = rng.gen_range(0, 100);
+
+        if rand_maybe <= 80 {
+            mutated[index_distribution.sample(&mut rng)] = random();
+            mutated[index_distribution.sample(&mut rng)] = random();
+        }
+
+        let mutate_cost = Self::hamming(&mutated);
+
+        if mutate_cost < self.cost || rand_maybe < 20 {
+            self.solution = mutated;
+            self.cost = mutate_cost;
+        }
+    }
+}
+
+impl std::fmt::Display for Chromosome {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(fmt, "\"{}\"", String::from_utf8_lossy(&self.solution))
+    }
+}
+
+impl PartialOrd for Chromosome {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cost.cmp(&other.cost))
+    }
+}
+
+impl Ord for Chromosome {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.cost.cmp(&other.cost)
+    }
+}
+
+impl PartialEq for Chromosome {
+    fn eq(&self, other: &Self) -> bool {
+        self.cost == other.cost
+    }
+}
+
+impl Mul for Chromosome {
+    type Output = (Self, Self);
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut father = self.solution;
+        let mut mother = rhs.solution;
+
+        assert_eq!(father.len(), mother.len());
+        let len = father.len();
+
+        let mut rng = thread_rng();
+        let cut_a: usize = rng.gen_range(0, len);
+        let cut_b: usize = rng.gen_range(0, len);
+        let min = cut_a.min(cut_b);
+        let max = cut_a.max(cut_b);
+
+        let mut son = vec![0; len];
+        let mut daughter = vec![0; len];
+
+        son[min..max].copy_from_slice(&mother[min..max]);
+        daughter[min..max].copy_from_slice(&father[min..max]);
+
+        mother.rotate_left(max);
+        father.rotate_left(max);
+
+        son[max..len].copy_from_slice(father.drain(0..(len - max)).as_slice());
+        son[0..min].copy_from_slice(father.drain(0..min).as_slice());
+
+        daughter[max..len].copy_from_slice(mother.drain(0..(len - max)).as_slice());
+        daughter[0..min].copy_from_slice(mother.drain(0..min).as_slice());
+
+        let son_cost = Chromosome::hamming(&son);
+        let daughter_cost = Chromosome::hamming(&daughter);
+
+        (
+            Chromosome {
+                solution: son,
+                cost: son_cost,
+            },
+            Chromosome {
+                solution: daughter,
+                cost: daughter_cost,
+            },
+        )
+    }
+}
+
+fn main() {
+    use indicatif::{ProgressBar, ProgressStyle};
+    use rayon::prelude::*;
+
+    let pb = ProgressBar::new_spinner();
+    pb.set_style(
+        ProgressStyle::default_spinner().template("{elapsed_precise} | {per_sec} | {wide_msg}"),
+    );
+
     let generation_size = 128;
     let parents_survive = generation_size / 10;
 
-    let mut parents: Vec<(Chromosome, u32)> =
-        std::iter::repeat_with(|| Chromosome::random(goal.len()))
-            .take(generation_size)
-            .map(|c| {
-                let score = c.distance(&goal);
-                (c, score)
-            })
-            .collect();
-    let mut children: Vec<(Chromosome, u32)> = Vec::with_capacity(generation_size);
+    let mut parents: Vec<Chromosome> = std::iter::repeat_with(|| Chromosome::random(GOAL.len()))
+        .take(generation_size)
+        .collect();
+    let mut children: Vec<Chromosome> = Vec::with_capacity(generation_size);
     let mut rng = thread_rng();
 
     for generation in 0.. {
-        if let Some(result) = parents.iter().find(|(_, score)| *score == 0) {
+        if let Some(result) = parents.iter().find(|c| c.cost == 0) {
             pb.finish_with_message(&format!(
                 "{} | {}",
                 generation,
-                result.0.to_string().escape_default()
+                result.to_string().escape_default()
             ));
             return;
         }
-        parents.par_sort_unstable_by(|a, b| a.1.cmp(&b.1));
 
         // copy the most successful ones
+        parents.par_sort_unstable();
         children.extend_from_slice(&parents[0..parents_survive]);
 
         // pair parents up
@@ -156,14 +188,13 @@ All mimsy were the borogoves,
         // FIXME: this is disgusting
         let remainder = generation_size - parents_survive;
         parents.shuffle(&mut rng);
-        let mut prospects: Vec<(Chromosome, u32)> = parents
+        let mut prospects: Vec<Chromosome> = parents
             .par_chunks_exact(2)
-            .map(|p| p[0].0.clone() * p[1].0.clone())
+            .map(|p| p[0].clone() * p[1].clone())
             .flat_map(|(a, b)| rayon::iter::once(a).chain(rayon::iter::once(b)))
             .map(|mut c| {
                 c.mutate();
-                let score = c.distance(&goal);
-                (c, score)
+                c
             })
             .collect();
         prospects.shuffle(&mut rng);
@@ -175,7 +206,7 @@ All mimsy were the borogoves,
         pb.set_message(&format!(
             "{} | {}",
             generation,
-            parents[0].0.to_string().escape_default()
+            parents[0].to_string().escape_default()
         ));
         pb.inc(1);
     }
