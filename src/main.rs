3706dc0ba7e2ec1a49e028902042bf580f38ee82
Bernardo Meurer <bernardo@meurer.org>
2020-12-05 16:53:46 -0800

    main: remove shuffle mutation

--- src/main.rs
+++ src/main.rs
@@ -29,10 +29,6 @@ impl Chromosome {
             self.solution[index_distribution.sample(&mut rng)] = random();
             self.solution[index_distribution.sample(&mut rng)] = random();
         }
-
-        if rand_maybe <= 14 {
-            self.solution.shuffle(&mut rng);
-        }
     }
 }
 
@@ -89,8 +85,44 @@ fn main() {
         ProgressStyle::default_spinner().template("{elapsed_precise} | {per_sec} | {wide_msg}"),
     );
 
-    let goal: Vec<u8> = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse feugiat, libero eget faucibus consequat, orci erat ornare neque, faucibus iaculis sapien nunc vitae arcu.".to_vec();
-    let generation_size = 50;
+    let goal: Vec<u8> = b"
+Twas brillig, and the slithy toves
+      Did gyre and gimble in the wabe:
+All mimsy were the borogoves,
+      And the mome raths outgrabe.
+
+\"Beware the Jabberwock, my son!
+      The jaws that bite, the claws that catch!
+Beware the Jubjub bird, and shun
+      The frumious Bandersnatch!\"
+
+He took his vorpal sword in hand;
+      Long time the manxome foe he sought-
+So rested he by the Tumtum tree
+      And stood awhile in thought.
+
+And, as in uffish thought he stood,
+      The Jabberwock, with eyes of flame,
+Came whiffling through the tulgey wood,
+      And burbled as it came!
+
+One, two! One, two! And through and through
+      The vorpal blade went snicker-snack!
+He left it dead, and with its head
+      He went galumphing back.
+
+\"And hast thou slain the Jabberwock?
+      Come to my arms, my beamish boy!
+O frabjous day! Callooh! Callay!\"
+      He chortled in his joy.
+
+'Twas brillig, and the slithy toves
+      Did gyre and gimble in the wabe:
+All mimsy were the borogoves,
+      And the mome raths outgrabe.
+    "
+    .to_vec();
+    let generation_size = 128;
     let parents_survive = generation_size / 10;
 
     let mut parents: Vec<(Chromosome, u32)> =
