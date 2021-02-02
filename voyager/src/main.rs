ca15e445c133526e272bdacc61965c5c3f1e687b
Bernardo Meurer <bernardo@meurer.org>
2021-02-02 02:04:08 -0800

    changed: fix voyager's color scheme

--- voyager/src/main.rs
+++ voyager/src/main.rs
@@ -8,21 +8,27 @@ use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
 const GRID_CELL_SIZE: i32 = 5;
 const GRID_WIDTH: i32 = 300;
 const GRID_HEIGHT: i32 = 400;
-const WINDOW_WIDTH: i32 = GRID_WIDTH * GRID_CELL_SIZE + 1;
-const WINDOW_HEIGHT: i32 = GRID_HEIGHT * GRID_CELL_SIZE + 1;
-const GRID_COLOR_BACKGROUND: Color = Color::RGBA(22, 22, 22, 255);
-const GRID_COLOR_LINE: Color = Color::RGBA(44, 44, 44, 255);
-const GRID_COLOR_ENTITY: Color = Color::RGBA(255, 255, 255, 255);
+const WINDOW_WIDTH: i32 = GRID_WIDTH * GRID_CELL_SIZE + GRID_CELL_SIZE;
+const WINDOW_HEIGHT: i32 = GRID_HEIGHT * GRID_CELL_SIZE + GRID_CELL_SIZE;
+const COLOR_BACKGROUND: Color = Color::RGBA(10, 14, 20, 255);
+const COLOR_ENTITY: Color = Color::RGBA(230, 180, 80, 255);
+const COLOR_PATH: Color = Color::RGBA(89, 194, 255, 255);
+
+fn gen_entities() -> Vec<Rect> {
+    let mut rng = thread_rng();
+    std::iter::repeat((
+        WINDOW_WIDTH - GRID_CELL_SIZE,
+        WINDOW_HEIGHT - GRID_CELL_SIZE,
+    ))
+    .map(|(x, y)| (rng.gen_range(0..x), rng.gen_range(0..y)))
+    .unique()
+    .take(50)
+    .map(|(x, y)| Rect::new(x, y, GRID_CELL_SIZE as u32, GRID_CELL_SIZE as u32))
+    .collect::<Vec<Rect>>()
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut rng = thread_rng();
-    let entities = std::iter::repeat((GRID_WIDTH, GRID_HEIGHT))
-        .map(|(x, y)| (rng.gen_range(0..x), rng.gen_range(0..y)))
-        .unique()
-        .take(15)
-        .map(|(x, y)| (x * GRID_CELL_SIZE, y * GRID_CELL_SIZE))
-        .map(|(x, y)| Rect::new(x, y, GRID_CELL_SIZE as u32, GRID_CELL_SIZE as u32))
-        .collect::<Vec<Rect>>();
+    let mut entities = gen_entities();
 
     let ctx = sdl2::init()?;
 
@@ -42,7 +48,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .position_centered()
         .build()?;
 
-    let mut canvas = window.into_canvas().present_vsync().build()?;
+    let mut canvas = window.into_canvas().build()?;
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -56,26 +62,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     keycode: Some(Keycode::Q),
                     ..
                 } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::R),
+                    ..
+                } => {
+                    entities = gen_entities();
+                }
                 _ => {}
             }
         }
-        // draw the background
-        canvas.set_draw_color(GRID_COLOR_BACKGROUND);
+        canvas.set_draw_color(COLOR_BACKGROUND);
         canvas.clear();
-        // draw the grid
-        canvas.set_draw_color(GRID_COLOR_LINE);
-        (0..(1 + GRID_WIDTH * GRID_CELL_SIZE))
-            .step_by(GRID_CELL_SIZE as usize)
-            .map(|x| canvas.draw_line((x, 0), (x, WINDOW_HEIGHT)))
-            .collect::<Result<(), String>>()?;
-        (0..(1 + GRID_HEIGHT * GRID_CELL_SIZE))
-            .step_by(GRID_CELL_SIZE as usize)
-            .map(|y| canvas.draw_line((0, y), (WINDOW_WIDTH, y)))
-            .collect::<Result<(), String>>()?;
 
-        canvas.set_draw_color(GRID_COLOR_ENTITY);
-
+        canvas.set_draw_color(COLOR_ENTITY);
         canvas.fill_rects(&entities)?;
+
+        canvas.set_draw_color(COLOR_PATH);
         entities
             .windows(2)
             .map(|p| (p[0].center(), p[1].center()))
